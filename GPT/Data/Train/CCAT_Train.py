5364f819b44f22e54c65e08e7a97b01226dd707f
ImXieHuang <13470825702@163.com>
2026-03-26 19:30:32 +0800

    每日例行更新20260327-02

--- GPT/Data/Train/CCAT_Train.py
+++ GPT/Data/Train/CCAT_Train.py
@@ -1,13 +1,15 @@
 import sys
 from pathlib import Path
-from typing import Any
+from typing import Callable
 udir = str(Path(__file__).parent.parent.parent)
 sys.path.append(udir)
 
-from Model.CCATransformer import CCATransformer, NewCCATransformer, get_meaning_of_tokens_for_, think_about_next_token_for_
+from Model.CCATransformer import CCATransformer, NewCCATransformer, get_meaning_of_sentence_for_, think_about_next_token_for_
+from Model.mathexpand import *
+from Model.Vector import *
 
 class CCAT_Trainer:
-    def __init__(self, dx: Any = None):
+    def __init__(self, dx: float = None):
         self.dx = dx or 1e-5
         self.models_dir = self.get_models_dir()
     
@@ -17,18 +19,21 @@ class CCAT_Trainer:
         models_dir.mkdir(exist_ok=True)
         return models_dir
 
-    def loss_gradient(self, token: str, inputs: list, loss: callable, ccat: CCATransformer):
-        pass
+    def loss_gradient(self, tokens: str, loss: Callable, ccat: CCATransformer):
+        original_data = ccat.database
+        bigQ = get_meaning_of_sentence_for_(ccat, tokens[:-1])
+        data = get_meaning_of_sentence_for_(ccat, tokens)
 
-    def up_backward():
-        pass
+        ccat.SoftInjection_to_(tokens[-1], bigQ, add(data, [self.dx for _ in data]))
+        add_loss = loss(get_meaning_of_sentence_for_(ccat, tokens))
+        ccat.database = original_data
+        
+        ccat.SoftInjection_to_(tokens[-1], bigQ, sub(data, [self.dx for _ in data]))
+        sub_loss = loss(get_meaning_of_sentence_for_(ccat, tokens))
+        ccat.database = original_data
 
-    def down_backward():
-        pass
-
-    def sub_backward():
-        pass
-
+        return div(div(sub(add_loss, sub_loss), 2), self.dx)
+    
     def ccat_trainer():
         pass
     
